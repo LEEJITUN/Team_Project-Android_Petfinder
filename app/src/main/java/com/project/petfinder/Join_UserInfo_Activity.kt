c609917e817b740c98f4412d6a25dd611c37538f
LeeCin <ekfhddl8@naver.com>
2021-10-20 14:55:51 +0900

    팻정보 ui 미리 업로드

--- app/src/main/java/com/project/petfinder/Join_UserInfo_Activity.kt
+++ app/src/main/java/com/project/petfinder/Join_UserInfo_Activity.kt
@@ -1,6 +1,9 @@
 package com.project.petfinder
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Join_UserInfo_Activity : AppCompatActivity() {
@@ -8,5 +11,13 @@ class Join_UserInfo_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_uer_info)
+
+        val btnRevercode : Button = findViewById(R.id.btnRevercode)
+
+        // 회원정보 입력 페이지 이동
+        btnRevercode.setOnClickListener(View.OnClickListener {
+            val intent = Intent(applicationContext, Join_User_PetInfo_Activity::class.java)
+            startActivity(intent)
+        })
     }
 }
---
+++ app/src/main/java/com/project/petfinder/Join_User_PetInfo_Activity.kt
@@ -0,0 +1,11 @@
+package com.project.petfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Join_User_PetInfo_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_user_pet_info)
+    }
+}
