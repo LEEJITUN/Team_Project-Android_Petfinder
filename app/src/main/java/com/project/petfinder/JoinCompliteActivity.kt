49850f3b43f95eca283396778a992defa5fc567f
LeeCin <ekfhddl8@naver.com>
2021-10-22 00:42:55 +0900

    메인페이지 생성

--- app/src/main/java/com/project/petfinder/JoinCompliteActivity.kt
+++ app/src/main/java/com/project/petfinder/JoinCompliteActivity.kt
@@ -1,11 +1,22 @@
 package com.project.petfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class JoinCompliteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_complite)
+
+        val btnRevercode : Button = findViewById(R.id.btnRevercode)
+
+        // 메인 화면으로 이동 ( MainActivity )
+        btnRevercode.setOnClickListener(View.OnClickListener {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
