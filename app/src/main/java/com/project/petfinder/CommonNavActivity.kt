83202f8ade1153b1f39e2657e3ca65a13f907d26
LeeCin <ekfhddl8@naver.com>
2021-10-21 11:22:15 +0900

    변경

---
+++ app/src/main/java/com/project/petfinder/CommonNavActivity.kt
@@ -0,0 +1,11 @@
+package com.project.petfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommonNavActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_common_nav)
+    }
+}
