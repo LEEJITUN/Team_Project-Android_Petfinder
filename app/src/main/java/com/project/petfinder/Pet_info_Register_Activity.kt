7057fabb812a43f0872165df5c71185d64969736
ryusumin <sumin5065@gmail.com>
2021-10-21 22:48:26 +0900

    수정 및 파일 생성 - register

---
+++ app/src/main/java/com/project/petfinder/Pet_info_Register_Activity.kt
@@ -0,0 +1,11 @@
+package com.project.petfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pet_info_Register_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pet_info_register)
+    }
+}
