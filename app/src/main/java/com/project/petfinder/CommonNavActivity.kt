bb0beae34aabcab73d25c907a6a17085d2756842
LeeCin <ekfhddl8@naver.com>
2021-10-22 00:54:26 +0900

    에러파일 지움

--- app/src/main/java/com/project/petfinder/CommonNavActivity.kt
+++ app/src/main/java/com/project/petfinder/CommonNavActivity.kt
@@ -3,34 +3,14 @@ package com.project.petfinder
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.project.petfinder.databinding.ActivityCommonNavBinding
+
 
 class CommonNavActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityCommonNavBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityCommonNavBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_common_nav)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
