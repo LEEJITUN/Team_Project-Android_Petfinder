18bcdf17795058b80775b6173e01b650943d531f
happinessbbin <jhb8991@hanmail.net>
2021-10-22 23:29:21 +0900

    My_info_Delete - ing...
    Signed-off-by: happinessbbin <jhb8991@hanmail.net>

---
+++ app/src/main/java/com/project/petfinder/My_info_Delete.kt
@@ -0,0 +1,11 @@
+package com.project.petfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class My_info_Delete : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_info_delete)
+    }
+}
