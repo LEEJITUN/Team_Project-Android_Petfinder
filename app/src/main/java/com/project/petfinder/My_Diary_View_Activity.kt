ad88eec9f506b773621aeb09932f82704c997e56
ryusumin <sumin5065@gmail.com>
2021-10-23 14:53:51 +0900

    Diary 파일 생성

---
+++ app/src/main/java/com/project/petfinder/My_Diary_View_Activity.kt
@@ -0,0 +1,11 @@
+package com.project.petfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class My_Diary_View_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_diary_view)
+    }
+}
