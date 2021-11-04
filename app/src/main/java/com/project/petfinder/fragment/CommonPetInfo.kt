e174c96ec3a82fb0dc81bac2283c3c9688405c15
ryusumin <sumin5065@gmail.com>
2021-11-04 19:11:46 +0900

    [수민] CommonPetInfo calender 버튼

--- app/src/main/java/com/project/petfinder/fragment/CommonPetInfo.kt
+++ app/src/main/java/com/project/petfinder/fragment/CommonPetInfo.kt
@@ -1,6 +1,7 @@
 package com.project.petfinder.fragment
 
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,45 +12,67 @@ import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import com.project.petfinder.Join_User_PetInfo_Activity
+import com.project.petfinder.PetInfoModifyFragment
+import com.project.petfinder.PetInfoRegisterFragment
 import com.project.petfinder.R
 import java.util.*
 
-class CommonPetInfo : Fragment(){
+class CommonPetInfo  {
 
-    lateinit var join_User_PetInfo_Activity: Join_User_PetInfo_Activity
-    var calendar = Calendar.getInstance()
-    var year = calendar.get(Calendar.YEAR)
-    var month = calendar.get(Calendar.MONTH)
-    var day = calendar.get(Calendar.DAY_OF_MONTH)
-    private lateinit var dialogFragment: DialogFragment
+//    lateinit var join_User_PetInfo_Activity: Join_User_PetInfo_Activity
+//    lateinit var petInfoModifyFragment: PetInfoModifyFragment
+//    lateinit var petInfoRegisterFragment: PetInfoRegisterFragment
+//
+//    private lateinit var dialogFragment: DialogFragment
+//
+//    companion object {
+//        fun newInstance(): CommonPetInfo = CommonPetInfo()
+//    }
+//
+//    override fun onAttach(context: Context) {
+//        super.onAttach(context)
+//
+//        join_User_PetInfo_Activity = context as Join_User_PetInfo_Activity
+//        petInfoModifyFragment = context as PetInfoModifyFragment
+//        petInfoRegisterFragment = context as PetInfoRegisterFragment
+//    }
 
-    companion object {
-        fun newInstance() : CommonPetInfo = CommonPetInfo()
-    }
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//    }
 
-        join_User_PetInfo_Activity = context as Join_User_PetInfo_Activity
-    }
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        var view: View = inflater.inflate(R.layout.common_pet_info, container, false)
+//    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+    fun calendar(context: Context, view: View) {
 
-        var view: View = inflater.inflate(R.layout.common_pet_info, container, false)
+        var calendar: Calendar = Calendar.getInstance()
+        var year: Int = calendar.get(Calendar.YEAR)
+        var month: Int = calendar.get(Calendar.MONTH)
+        var day: Int = calendar.get(Calendar.DAY_OF_MONTH)
+
         var btn_calender = view?.findViewById(R.id.btn_calender) as Button
-        var btn_pet_brith = view?.findViewById(R.id.btn_pet_brith) as TextView
+        var tv_pet_brith = view?.findViewById(R.id.tv_pet_brith) as TextView
 
-
-        btn_calender.setOnClickListener  {
-            join_User_PetInfo_Activity.openDialog()
-        }
-
-        return view
-
+        btn_calender.setOnClickListener(View.OnClickListener {
+            val Cal = Calendar.getInstance()
+            day = Cal[Calendar.DATE]
+            month = Cal[Calendar.MONDAY]
+            year = Cal[Calendar.YEAR]
+            val datePickerDialog = DatePickerDialog(
+                context, android.R.style.Theme_DeviceDefault_Dialog,
+                { view, year, month, dayOfMonth ->
+                    tv_pet_brith.setText( "$year-$month-$dayOfMonth")
+                },
+                year, month + 1, day
+            )
+            datePickerDialog.show()
+        })
     }
-
-
 }
 
